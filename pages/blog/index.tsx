c283be7332dd9fecb091447fa4096081afde1c3d
Joschua H <joschua.hass.sh@gmail.com>
2023-02-14 20:51:44 +0100

    oh baby that's hot

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -16,6 +16,87 @@ import {
   Title
 } from "@mantine/core";
 
+import KiaEv6DriftImage from "../../public/img/2023-ev6-gt-action-1660755931.jpg";
+import TeslaAutopilotImage from "../../public/img/tesla-model-s-autopilot-software-70.jpeg";
+import RivianR1TImage from "../../public/img/RivianR1T.jpeg";
+import FreewireImage from "../../public/img/FreewireChargingStation.jpeg"
+import Image from "next/image";
+
+interface Post {
+  id: number;
+
+  title: string;
+
+  subtitle: string;
+
+  published: Date,
+
+  readTime: number,
+
+  author: {
+    firstName: string;
+
+    lastName: string;
+  },
+
+  imageUrl: string;
+
+  objectPosition?: string;
+}
+
+const posts: Post[] = [
+  {
+    id: 1,
+    title: "Using the private Kia API",
+    subtitle: "Kia, when will you make our life easier?",
+    author: {
+      firstName: "NextGen",
+      lastName: "Drive"
+    },
+    published: new Date(2022, 1, 3),
+    readTime: 2,
+    imageUrl: KiaEv6DriftImage.src
+  },
+  {
+    id: 2,
+    title: "Tesla works on v4 Hardware",
+    subtitle: "Autopilot gettin' spicy",
+    author: {
+      firstName: "NextGen",
+      lastName: "Drive"
+    },
+    published: new Date(2022, 1, 9),
+    readTime: 4,
+    imageUrl: TeslaAutopilotImage.src
+  },
+  {
+    id: 3,
+    title: "Rivian suffers from production difficulties",
+    subtitle: "God save the Rivian",
+    author: {
+      firstName: "NextGen",
+      lastName: "Drive"
+    },
+    published: new Date(2022, 1, 7),
+    readTime: 2,
+    imageUrl: RivianR1TImage.src,
+    objectPosition: "0% 50%"
+  },
+  {
+    id: 4,
+    title: "Freewire shows us how to not care about the grid",
+    subtitle: "Juice 'em up",
+    author: {
+      firstName: "NextGen",
+      lastName: "Drive"
+    },
+    published: new Date(2022, 0, 29),
+    readTime: 6,
+    imageUrl: FreewireImage.src,
+    objectPosition: "0% 50%"
+  }
+]
+
 export default function NewLandingPage() {
   return (
     <div style={{background: "black"}}>
@@ -29,36 +110,12 @@ export default function NewLandingPage() {
         <div />
       </div>
       <Container size={1300}>
-        <Box display="flex" p="xl" sx={{background: "#212121", height: "60vh", flexDirection: "column", justifyContent: "space-between"}}>
-          <Box />
-          <Box style={{maxWidth: "650px"}}>
-            <Title order={1}>Using the private Kia API <span style={{color: "#797979"}}>Kia, when will you make our life easier?</span></Title>
-            <Text size="sm" my="lg" color="dimmed">by Joschua Haß - January 30, 2023 - 2 minute read</Text>
-          </Box>
-        </Box>
+        <HeaderNewsItem post={posts[0]} />
         <Space h="xl" />
         <SimpleGrid cols={3}>
-          <Box display="flex" p="xl" sx={{background: "#212121", height: "60vh", flexDirection: "column", justifyContent: "space-between"}}>
-            <Box />
-            <Box style={{maxWidth: "650px"}}>
-              <Title order={2}>Using the private Kia API <span style={{color: "#797979"}}>Kia, when will you make our life easier?</span></Title>
-              <Text size="sm" my="lg" color="dimmed">by Joschua Haß - January 30, 2023 - 2 minute read</Text>
-            </Box>
-          </Box>
-          <Box display="flex" p="xl" sx={{background: "#212121", height: "60vh", flexDirection: "column", justifyContent: "space-between"}}>
-            <Box />
-            <Box style={{maxWidth: "650px"}}>
-              <Title order={2}>Using the private Kia API <span style={{color: "#797979"}}>Kia, when will you make our life easier?</span></Title>
-              <Text size="sm" my="lg" color="dimmed">by Joschua Haß - January 30, 2023 - 2 minute read</Text>
-            </Box>
-          </Box>
-          <Box display="flex" p="xl" sx={{background: "#212121", height: "60vh", flexDirection: "column", justifyContent: "space-between"}}>
-            <Box />
-            <Box style={{maxWidth: "650px"}}>
-              <Title order={2}>Using the private Kia API <span style={{color: "#797979"}}>Kia, when will you make our life easier?</span></Title>
-              <Text size="sm" my="lg" color="dimmed">by Joschua Haß - January 30, 2023 - 2 minute read</Text>
-            </Box>
-          </Box>
+          <MediumNewsItem post={posts[1]} />
+          <MediumNewsItem post={posts[2]} />
+          <MediumNewsItem post={posts[3]} />
         </SimpleGrid>
         <Space h={42} />
         <SimpleGrid cols={2}>
@@ -80,6 +137,49 @@ function SmallNewsItem() {
       <Box mx="xl" style={{}}>
         <Title order={3}>Using the private Kia API <span style={{color: "#797979"}}>Kia, when will you make our life easier?</span></Title>
         <Text size="sm" my="sm" color="dimmed">by Joschua Haß - January 30, 2023</Text>
+      </Box>
+    </Box>
+  )
+}
+
+interface HeaderNewsItemProps {
+  post: Post;
+}
+function HeaderNewsItem({post}: HeaderNewsItemProps) {
+  const authorName = `${post.author.firstName} ${post.author.lastName}`;
+  const publishedDate = post.published.toDateString();
+
+  return (
+    <Box display="flex" sx={{height: "60vh", position: "relative"}}>
+      <Image alt="news title image" src={post.imageUrl} fill={true} style={{objectFit: "cover", objectPosition: "50% 50%"}} priority />
+      <Box p="xl" sx={{background: "rgba(0,0,0,0.5)", height: "100%", width: "100%", display: "flex", flexDirection: "column", justifyContent: "end", zIndex: 2}}>
+        <Box style={{maxWidth: "650px"}}>
+          <Title order={1} style={{textShadow: "1px 1px 4px black"}}>
+            {post.title + ' '}
+            <span style={{color: "#b2b2b2", fontWeight: 400}}>{post.subtitle}</span>
+          </Title>
+          <Text size="sm" my="lg" color="#b2b2b2">by {authorName} - {publishedDate} - {post.readTime} minute read</Text>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+function MediumNewsItem({post}: HeaderNewsItemProps) {
+  const authorName = `${post.author.firstName} ${post.author.lastName}`;
+  const publishedDate = post.published.toDateString();
+
+  return (
+    <Box display="flex" sx={{height: "60vh", position: "relative"}}>
+      <Image alt="news title image" src={post.imageUrl} fill={true} style={{objectFit: "cover", objectPosition: post.objectPosition}} priority />
+      <Box p="xl" style={{background: "rgba(0,0,0,0.4)", height: "100%", width: "100%", display: "flex", flexDirection: "column", justifyContent: "end", zIndex: 2}}>
+        <Box style={{maxWidth: "650px"}}>
+          <Title order={2} style={{textShadow: "1px 1px 4px black"}}>
+            {post.title + ' '}
+            <span style={{color: "#b2b2b2", fontWeight: 400}}>{post.subtitle}</span>
+          </Title>
+          <Text size="sm" my="lg" color="#b2b2b2">by {authorName} - {publishedDate} - {post.readTime} minute read</Text>
+        </Box>
       </Box>
     </Box>
   )
